66ad6cc2dde16c9b398bf967399a404ef0427731
SkinnyDevi <skinnydevi.dev@gmail.com>
2023-09-01 23:55:32 +0100

    Playing around with ui

--- core/ui/main.py
+++ core/ui/main.py
@@ -20,8 +20,19 @@ def mount_ui(main_frame: tk.Tk):
     title_frame.pack(pady=15, padx=10, side=tk.TOP, anchor="w")
 
     subtitle_frame = tk.Frame(main_frame)
-    subtitle = components.frame_text(
-        subtitle_frame, "Downloaded mods", 16, underline=10
-    )
+    subtitle = components.frame_text(subtitle_frame, "Downloaded mods", 16)
     subtitle.pack()
     subtitle_frame.pack(pady=0, padx=10, side=tk.TOP, anchor="w")
+
+    mods_frame = tk.Frame(main_frame)
+
+    mod_frame = tk.Frame(mods_frame)
+    mod_title = components.frame_text(
+        mod_frame,
+        "Mod Title",
+        24,
+    )
+
+    mod_title.pack()
+    mod_frame.pack()
+    mods_frame.pack(pady=20, padx=10, side=tk.TOP, anchor="w")
